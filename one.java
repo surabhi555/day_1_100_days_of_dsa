83e13b5d1247cd5cd598ff720da517db43296d8e
surabhi555 <99637606+surabhi555@users.noreply.github.com>
2024-09-16 11:20:17 +0530

    Add files via upload

--- one.java
+++ one.java
@@ -1,12 +1,15 @@
-import java.util.*;
-class one{
-    
-public static void main(String[]args){
-    int i=1;
-    while(i<=10){
-        System.out.print(i+" ");
-        i++;
+// *
+// * *
+// * * *
+// * * * *
+
+public class one{
+    public static void main(String []args){
+        for(int line=1;line<=5;line++){
+            for(int  star=1;star<=line;star++){
+                System.out.print("*" +" ");
+            }
+            System.out.println();
+        }
     }
-    
-}
 }
--- two.java
+++ two.java
@@ -1,13 +1,20 @@
-import java.util.*;
-public class two {
+// ****
+//*** 
+//** 
+//* 
+
+import java.util.Scanner;
+
+public class two{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("'enter the no of line you want to enter'");
         int n=sc.nextInt();
-        int i=1;
-        while(i<=n){
-            System.out.print(i+" ");
-            i++;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
         }
-        System.out.println();
     }
 }
